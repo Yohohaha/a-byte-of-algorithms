977f2fd3d50c60a86261da09bb8fe284b4999f39
Yohohaha <390342156@qq.com>
2020-08-27 19:49:25 +0800

    修改二叉树美化算法

--- src/main/java/com/github/yohohaha/algorithms/学点算法十三_二叉树打印美化算法.java
+++ src/main/java/com/github/yohohaha/algorithms/学点算法十三_二叉树打印美化算法.java
@@ -18,52 +18,20 @@ public class 学点算法十三_二叉树打印美化算法 {
 
     @Test
     public void testReverseOrder() {
-        BinaryTreeNode<Integer> root = convertArray2Tree(new Integer[]{1, 2, 3, 4, 5});
-        Deque<Deque<BinaryTreeNode<Integer>>> stack = reverseOrder(root);
-        Iterator<Deque<BinaryTreeNode<Integer>>> iter = stack.descendingIterator();
-        while (iter.hasNext()) {
-            Deque<BinaryTreeNode<Integer>> deque = iter.next();
-            for (BinaryTreeNode<Integer> integerBinaryTreeNode : deque) {
-                System.out.println(integerBinaryTreeNode.data);
-            }
-        }
+
     }
 
-    private static <E extends Comparable<E>> Deque<Deque<BinaryTreeNode<E>>> reverseOrder(BinaryTreeNode<E> root) {
-        if (root == null) {
-            return null;
-        }
-        // 外部栈
-        Deque<Deque<BinaryTreeNode<E>>> stack = new ArrayDeque<>();
-        // 内部栈
-        Deque<BinaryTreeNode<E>> deque = new ArrayDeque<>(1);
-        // 遍历队列
-        Deque<BinaryTreeNode<E>> queue = new ArrayDeque<>();
-        BinaryTreeNode<E> currentNode = root;
-        deque.push(currentNode);
-        stack.push(deque);
-        deque = new ArrayDeque<>(deque.size() * 2);
-        do {
-            boolean isLast = false;
-            if (currentNode.rightChild != null) {
-                queue.offer(currentNode.rightChild);
-                deque.push(currentNode.rightChild);
-                if (currentNode.parent == stack.peekFirst().peekLast()) {
-                    isLast = true;
-                }
-            }
-            if (currentNode.leftChild != null) {
-                queue.offer(currentNode.leftChild);
-                deque.push(currentNode.leftChild);
-                if (currentNode.parent == stack.peekFirst().peekLast()) {
-                    isLast = true;
-                }
-            }
-            if (isLast) {
-                stack.push(deque);
-                deque = new ArrayDeque<>(deque.size() * 2);
-            }
-        } while ((currentNode = queue.poll()) != null);
-        return stack;
+    public static <E extends Comparable<E>> void printBeautifulBinaryTree(BinaryTreeNode<E> root) {
+        // 定义元素间间隔nodeInterval
+        // 定义树层级间隔levelInterval
+        // 获取高度h和最大的元素宽度maxNodeLen
+
+        // 根据上述两个条件获取画布长和宽
+        // 长为(maxNodeLen + nodeInterval) * 2 ^ (h-1)
+        // 宽为(levelInterval + 1) * (h - 1) + 1
+
+
+
+
     }
 }
